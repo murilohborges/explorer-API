126eaab4dfbfa33f3a84d9c420ae0ac98a32928f
murilo <mhenrique.mb@gmail.com>
2024-02-18 13:33:41 -0300

    first commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,14 @@
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password} = request.body;
+    
+    if(!name){
+      throw new AppError("Nome é obrigatório");
+    }
+
+    response.json({ name, email, password});
+  }
+}
+
+module.exports = UsersController
