abb0afcf726f68263418b81b44e76c3aaf5b5d6b
murilo <mhenrique.mb@gmail.com>
2024-02-19 11:01:02 -0300

    creating migrations and installing knex

---
+++ src/database/knex/migrations/20240219004157_createPlates.js
@@ -0,0 +1,15 @@
+exports.up = knex => knex.schema.createTable("plates", table => {
+  table.increments("id");
+  table.text("avatar");
+  table.text("name");
+  table.text("category");
+  table.text("price");
+  table.text("description");
+  table.integer("user_id").references("id").inTable("users");
+
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+});
+
+
+exports.down = knex => knex.schema.dropTable("plates");
