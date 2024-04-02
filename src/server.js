a8630f24bbe1a3f6be4bb392e7ef2630fcc90f42
murilo <mhenrique.mb@gmail.com>
2024-04-02 10:14:31 -0300

    installing cors

--- src/server.js
+++ src/server.js
@@ -6,12 +6,15 @@ const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError");
 
+const cors = require("cors");
+
 const uploadConfig = require('./configs/upload');
 
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
