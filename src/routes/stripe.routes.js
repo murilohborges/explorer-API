f319c3819cb8c8163dd1fb730f67c9d5a0836d24
murilo <mhenrique.mb@gmail.com>
2024-10-04 10:22:00 -0300

    creating for StripeController

---
+++ src/routes/stripe.routes.js
@@ -0,0 +1,14 @@
+const { Router, request, response } = require("express");
+const StripeController = require("../controllers/StripeController.js");
+const stripeRoutes = Router();
+const ensureAuthenticated = require("../middleware/ensureAuthenticated.js");
+const verifyUserAuthorization = require("../middleware/verifyUserAuthorization.js");
+
+const stripeController = new StripeController();
+
+// stripeRoutes.use(ensureAuthenticated);
+
+stripeRoutes.post("/", stripeController.create);
+
+
+module.exports = stripeRoutes;
