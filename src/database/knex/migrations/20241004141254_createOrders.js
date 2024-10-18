0a2057297e4e032cb08c0ca78767251a9e3dd7c4
murilo <mhenrique.mb@gmail.com>
2024-10-18 11:36:15 -0300

    adding user_id column

--- src/database/knex/migrations/20241004141254_createOrders.js
+++ src/database/knex/migrations/20241004141254_createOrders.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable("orders", table => {
   table.increments("id");
   table.text("status");
   table.text("details");
+  table.integer("user_id_who_ordered").references("id").inTable("users");
   table.timestamp("created_at").default(knex.fn.now());
 });
 
