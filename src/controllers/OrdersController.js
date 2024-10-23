49c540e0586894ee9df9f2415f2803cb8df5038c
murilo <mhenrique.mb@gmail.com>
2024-10-23 11:51:02 -0300

    correct lines and creating index and update methods

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -5,7 +5,7 @@ const moment = require('moment-timezone');
 class OrdersController {
   async create(request, response){
     const { details } = request.body;
-
+    const user_id = request.user.id
     
     let detailsArray = details.map((item) => {
       let itemArray = `${item.plate_amount} x ${item.plate_title}`
@@ -14,11 +14,12 @@ class OrdersController {
 
     const [order_id] = await knex('orders').insert({
       status: "pending",
-      details: String(detailsArray.join(', '))
+      details: String(detailsArray.join(', ')),
+      user_id_who_ordered: user_id
     });
 
     const order = await knex('orders').where({ id: order_id }).first();
-
+    
     const adjustedTime = moment.utc(order.created_at).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss');
 
     await knex('orders')
@@ -29,5 +30,33 @@ class OrdersController {
 
     return response.status(200).json({ order_id });
   }
+
+  async index(request, response){
+    const user_id = request.user.id
+    const user_role = request.user.role
+
+    let orders;
+
+    if(user_role == 'customer'){
+      orders = await knex("orders").where("user_id_who_ordered", user_id);
+    }
+    if(user_role == 'admin'){
+      orders = await knex("orders");
+    }
+    
+    return response.status(200).json({ orders });
+  }
+
+  async update(request, response){
+    const { orders } = request.body;
+
+    for(let i = 0; i < orders.length; i++){
+
+      await knex('orders').where({ id: orders[i].id }).update({ status: orders[i].status });
+    }
+
+    return response.status(200).json({});
+  }
+  
 }
 module.exports = OrdersController
