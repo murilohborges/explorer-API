066205a5575969a2a0476a02b65c473ed71e19b3
murilo <mhenrique.mb@gmail.com>
2024-10-25 10:32:10 -0300

    adding methods and adjusting code

--- src/controllers/WebhookController.js
+++ src/controllers/WebhookController.js
@@ -1,6 +1,7 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 const AppError = require("../utils/AppError");
-const knex = require("../database/knex");
+const authConfig = require("../configs/auth");
+const { verify, sign } = require("jsonwebtoken");
 
 class WebhookController {
   async create(request, response){
@@ -10,18 +11,55 @@ class WebhookController {
     
     try {
       event = stripe.webhooks.constructEvent(request.body, sig, `${process.env.WEBHOOK_SIGNING_SECRET}`);
-    } catch (err) {
-      console.error('Erro de verificação de assinatura:', err.message);
-      return response.status(400).send(`Webhook Error: ${err.message}`);
+
+    } catch (e) {
+      throw new AppError('Erro de verificação de assinatura:');
     }
 
     if (event.type === 'payment_intent.succeeded') {
       const paymentIntent = event.data.object;
-      console.log("SUCESSO")
-      // const paymentToken
+
+      const tokenPayload = {
+        paymentIntentId: paymentIntent.id,
+        amount: paymentIntent.amount,
+        currency: paymentIntent.currency,
+        status: paymentIntent.status,
+      };
+
+      const { secret } = authConfig.jwt;
+      const paymentToken = sign(tokenPayload, secret, {
+        expiresIn: '60s'
+      })
+
+      response.cookie("paymentToken", paymentToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 1 * 30 * 1000
+      })
+
     }
-  
-    response.status(200).send('Webhook recebido com sucesso');
+    
+    return response.status(201).json({});
+  }
+
+  async index(request, response){
+    const authHeader = request.headers;
+    const tokens = authHeader.cookie.split(";");
+    const paymentToken = tokens[1].split("paymentToken=")[1].trim();
+
+    if (!paymentToken) {
+      throw new AppError('Token de pagamento não encontrado.');
+    }
+
+    try {
+      // Verifique o token (por exemplo, usando JWT)
+      const { paymentIntentId } = verify(paymentToken, authConfig.jwt.secret);
+      return response.json({ status: 'ok', paymentIntentId });
+
+    } catch (error) {
+      throw new AppError('Token de pagamento inválido ou expirado.');
+    }
   }
   
 }
