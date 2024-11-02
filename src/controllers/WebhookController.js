ef3afc7e3ee0c322d875f183edf2889d1f303185
murilo <mhenrique.mb@gmail.com>
2024-11-02 19:26:34 -0300

    testing two events of stripe webhook

--- src/controllers/WebhookController.js
+++ src/controllers/WebhookController.js
@@ -11,34 +11,34 @@ class WebhookController {
     try {
       const payload = request.body.toString('utf8');
       event = stripe.webhooks.constructEvent(payload, sig, `${process.env.WEBHOOK_SIGNING_SECRET}`);
-      
     } catch (e) {
       throw new AppError(`Erro de verificação de assinatura:`, e.message);
     }
+    console.log("event:")
+    console.log(event)
+
+    console.log("event.data:")
+    console.log(event.data)
+
     
-    if (event.type === 'payment_intent.succeeded') {
-      const paymentIntent = event.data.object;
+    // if (event.type === 'payment_intent.succeeded') {
+    //   // const paymentIntent = event.data.object;
       
-      const tokenPayload = {
-        paymentIntentId: paymentIntent.id,
-        amount: paymentIntent.amount,
-        currency: paymentIntent.currency,
-        status: paymentIntent.status,
-      };
+    //   // const tokenPayload = {
+    //   //   paymentIntentId: paymentIntent.id,
+    //   //   amount: paymentIntent.amount,
+    //   //   currency: paymentIntent.currency,
+    //   //   status: paymentIntent.status,
+    //   // };
+
+    //   const tokenPayload = { teste: 'asjndkjan' }
       
-      const { secret } = authConfig.jwt;
-      const paymentToken = sign(tokenPayload, secret, {
-        expiresIn: '60s'
-      })
+    //   const { secret } = authConfig.jwt;
+    //   const paymentToken = sign(tokenPayload, secret, {
+    //     expiresIn: '60s'
+    //   })
 
-      response.cookie("paymentToken", paymentToken, {
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-        maxAge: 60 * 1000
-      })
-
-    }
+    // }
     
     return response.status(201).json({ message: "Token de pagamento gerado com sucesso" });
   }
