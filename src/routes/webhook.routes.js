dd05da2cc0da50ffaffd99074dd256254c18df3b
murilo <mhenrique.mb@gmail.com>
2024-12-20 17:59:33 -0300

    removing lines

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,8 +1,6 @@
 const { Router, request, response } = require("express");
 const WebhookController = require("../controllers/WebhookController.js");
 const webhookRoutes = Router();
-const ensureAuthenticated = require("../middleware/ensureAuthenticated.js");
-const verifyUserAuthorization = require("../middleware/verifyUserAuthorization.js");
 
 const webhookController = new WebhookController();
 
